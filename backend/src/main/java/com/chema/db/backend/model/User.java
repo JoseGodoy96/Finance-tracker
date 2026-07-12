8cb88bcaac960cd278fb06caaf3e9a09327867b7
Jose Maria <josemariagodoy1996@gmail.com>
2026-07-12 20:20:28 +0100

    <feat>(User.java): Modificaciones para añadir createdAt, la distancia de la password

--- backend/src/main/java/com/chema/db/backend/model/User.java
+++ backend/src/main/java/com/chema/db/backend/model/User.java
@@ -1,4 +1,40 @@
 package com.chema.db.backend.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false, unique = true)
+    @Size(max = 50, message = "Username cannot exceed 50 characters")
+    private String username;
+
+    @NotBlank
+    @Email
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
 }
