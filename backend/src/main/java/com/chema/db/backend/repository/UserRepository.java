b39b1de6c9a766bab6fb41ae263cfefcd07c8482
Jose Maria <josemariagodoy1996@gmail.com>
2026-07-13 18:12:27 +0100

    <feat>(repository.p, UserRepository.java): UserRepository basico con una pequeña modificacion para usar JWT

---
+++ backend/src/main/java/com/chema/db/backend/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.chema.db.backend.repository;
+
+import com.chema.db.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
