77c0c574ff3d65c5af8ef888ca85eb853aa4b121
Jose Maria <josemariagodoy1996@gmail.com>
2026-07-19 17:10:23 +0100

    <feat>(TransactionService.java, TransactionRepository.java): Comienzo de TransactionService creados los metodos getAllUser y findByIdForUser y delete modificacion de TransictionRepository que se me habia olvidado

--- backend/src/main/java/com/chema/db/backend/repository/TransactionRepository.java
+++ backend/src/main/java/com/chema/db/backend/repository/TransactionRepository.java
@@ -1,7 +1,11 @@
 package com.chema.db.backend.repository;
 
 import com.chema.db.backend.model.Transaction;
+import com.chema.db.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findByUser(User user);
 }
--- backend/src/main/java/com/chema/db/backend/service/TransactionService.java
+++ backend/src/main/java/com/chema/db/backend/service/TransactionService.java
@@ -1,4 +1,53 @@
 package com.chema.db.backend.service;
 
+import com.chema.db.backend.exception.ResourceNotFoundException;
+import com.chema.db.backend.model.Transaction;
+import com.chema.db.backend.model.User;
+import com.chema.db.backend.repository.TransactionRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+@Service
 public class TransactionService {
+
+    private final TransactionRepository transactionRepository;
+
+    public TransactionService(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+    public List<Transaction> findAllForUser(User user) {
+        return transactionRepository.findByUser(user);
+    }
+
+    public Transaction findByIdForUser(Long id, User user) {
+        Transaction transaction = transactionRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Transaction", id));
+
+        if (!transaction.getUser().getId().equals(user.getId())) {
+            throw new RuntimeException("You cannot access this transaction");
+        }
+
+        return transaction;
+    }
+
+    public Transaction createForUser(Transaction transaction, Long categoryId, User user) {
+
+    }
+
+    public Transaction updateForUser(Long id, Transaction updated, Long categoryId, User user) {
+
+    }
+
+    void deleteForUser(Long id, User user) {
+        Transaction transaction = transactionRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Transaction", id));
+
+        if (transaction.getUser() == null || !transaction.getUser().getId().equals(user.getId())) {
+            throw new RuntimeException("You cannot delete this transaction");
+        }
+        transactionRepository.delete(transaction);
+    }
 }
