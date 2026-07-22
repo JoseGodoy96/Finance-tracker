bf6fb2e80d466999c9b2bdf092cc063b73e10c51
Jose Maria <josemariagodoy1996@gmail.com>
2026-07-22 23:12:12 +0100

    <feat>(UserService.java): Register done

--- backend/src/main/java/com/chema/db/backend/service/UserService.java
+++ backend/src/main/java/com/chema/db/backend/service/UserService.java
@@ -1,4 +1,26 @@
 package com.chema.db.backend.service;
 
+import com.chema.db.backend.model.User;
+import com.chema.db.backend.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User register(User user) {
+
+        user.setPassword(
+                passwordEncoder.encode(user.getPassword())
+        );
+        return userRepository.save(user);
+    }
 }
