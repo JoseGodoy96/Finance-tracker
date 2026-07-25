c93f56877ff9c34a73c4fb5d2c1f5dc93f89df9c
Jose Maria <josemariagodoy1996@gmail.com>
2026-07-25 21:44:12 +0100

    <feat>(UserController.java): Creado el register de userController

--- backend/src/main/java/com/chema/db/backend/controller/UserController.java
+++ backend/src/main/java/com/chema/db/backend/controller/UserController.java
@@ -1,4 +1,25 @@
 package com.chema.db.backend.controller;
 
+import com.chema.db.backend.model.User;
+import com.chema.db.backend.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public User register(@Valid @RequestBody User user) {
+        return userService.register(user);
+    }
 }
