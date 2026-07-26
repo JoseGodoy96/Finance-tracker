656a55d39f935a342b7a74a33eed3acb179dbb5c
Jose Maria <josemariagodoy1996@gmail.com>
2026-07-26 16:45:27 +0100

    <feat>(TransactionController.java): Primer endpoint realizado

--- backend/src/main/java/com/chema/db/backend/controller/TransactionController.java
+++ backend/src/main/java/com/chema/db/backend/controller/TransactionController.java
@@ -1,4 +1,31 @@
 package com.chema.db.backend.controller;
 
+import com.chema.db.backend.model.Transaction;
+import com.chema.db.backend.model.User;
+import com.chema.db.backend.service.TransactionService;
+import com.chema.db.backend.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/transactions")
 public class TransactionController {
+
+    private final TransactionService transactionService;
+    private final UserService userService;
+
+    public TransactionController(TransactionService transactionService, UserService userService) {
+        this.transactionService = transactionService;
+        this.userService = userService;
+    }
+
+     @GetMapping
+    public List<Transaction> findAll(@RequestParam String username) {
+        User user = userService.findByUsername(username);
+        return transactionService.findAllForUser(user);
+    }
 }
