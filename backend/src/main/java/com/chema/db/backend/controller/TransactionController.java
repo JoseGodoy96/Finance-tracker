d6604fc4210169c3ce9d6e8e3bbcfeb7fd984e57
Jose Maria <josemariagodoy1996@gmail.com>
2026-07-28 11:22:37 +0100

    <feat>(application.properties, pom.xml): Añadir las nuevas dependencias a pom.xml y las configuracion de JWT

--- backend/src/main/java/com/chema/db/backend/controller/TransactionController.java
+++ backend/src/main/java/com/chema/db/backend/controller/TransactionController.java
@@ -12,7 +12,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/transactions")
 public class TransactionController {
-
     private final TransactionService transactionService;
     private final UserService userService;
 
