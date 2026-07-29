08daf33c1b10e88ef7dabe03bfedf8a7d94bb1b4
Jose Maria <josemariagodoy1996@gmail.com>
2026-07-29 14:55:59 +0100

    <feat>(AuthResponse.java): Finalizacion de AuthResponse.java

--- backend/src/main/java/com/chema/db/backend/dto/AuthResponse.java
+++ backend/src/main/java/com/chema/db/backend/dto/AuthResponse.java
@@ -1,4 +1,13 @@
 package com.chema.db.backend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class AuthResponse {
+
+    private String token;
 }
