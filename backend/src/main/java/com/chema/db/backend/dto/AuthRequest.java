e443f817fae42129ec63d69a0d35404e957236d8
Jose Maria <josemariagodoy1996@gmail.com>
2026-07-29 14:49:35 +0100

    <feat>(AuthRequest.java): Finalizacion de AuthRequest.java

--- backend/src/main/java/com/chema/db/backend/dto/AuthRequest.java
+++ backend/src/main/java/com/chema/db/backend/dto/AuthRequest.java
@@ -1,4 +1,16 @@
 package com.chema.db.backend.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class AuthRequest {
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
 }
