68a38ccbb654c2445f6d0ddd7fa2cfdde982b8a4
Jose Maria <josemariagodoy1996@gmail.com>
2026-07-29 14:28:22 +0100

    <feat>(JwtService.java): Mejora de JwtService.java

---
+++ backend/src/main/java/com/chema/db/backend/service/JwtService.java
@@ -0,0 +1,21 @@
+package com.chema.db.backend.service;
+
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private  String secret;
+
+    @Value("${jwt.expiration-ms}")
+    private long expirationMs;
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+}
