73e1cf5069a48c2991bfd75892c2a121532d180e
Jose Maria <josemariagodoy1996@gmail.com>
2026-08-05 22:05:34 +0100

    <feat>(UserResponse.java): creacion de el dto UserResponse

--- backend/src/main/java/com/chema/db/backend/dto/UserResponse.java
+++ backend/src/main/java/com/chema/db/backend/dto/UserResponse.java
@@ -1,4 +1,19 @@
 package com.chema.db.backend.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public class UserResponse {
+
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    private LocalDateTime createdAt;
 }
