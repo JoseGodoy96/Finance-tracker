5b0054ea8703485fc5c18d4e027cd144a5f16286
Jose Maria <josemariagodoy1996@gmail.com>
2026-08-06 22:20:58 +0100

    <feat>(CategoryResponse.java): Final de CategoryResponse

--- backend/src/main/java/com/chema/db/backend/dto/CategoryResponse.java
+++ backend/src/main/java/com/chema/db/backend/dto/CategoryResponse.java
@@ -1,4 +1,15 @@
 package com.chema.db.backend.dto;
 
+import com.chema.db.backend.model.TransactionType;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CategoryResponse {
+
+    private Long id;
+    private String name;
+    private TransactionType type;
+    private boolean system;
 }
