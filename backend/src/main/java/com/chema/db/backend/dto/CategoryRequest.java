285ef90d2cc11ef9849dfeb9899635e5932e52c2
Jose Maria <josemariagodoy1996@gmail.com>
2026-08-06 22:18:09 +0100

    <feat>(CategoryRequest.java): Final de CategoryRequest

--- backend/src/main/java/com/chema/db/backend/dto/CategoryRequest.java
+++ backend/src/main/java/com/chema/db/backend/dto/CategoryRequest.java
@@ -1,5 +1,20 @@
 package com.chema.db.backend.dto;
 
+import com.chema.db.backend.model.TransactionType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CategoryRequest {
 
+    @NotBlank
+    @Size(max = 50)
+    private String name;
+
+    @NotNull
+    private TransactionType type;
 }
