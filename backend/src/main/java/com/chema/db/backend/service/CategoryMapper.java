f19ad34c1abb07c758ff281eddd8dafddc3cbbd0
Jose Maria <josemariagodoy1996@gmail.com>
2026-08-08 11:10:58 +0100

    <feat>(CategoryMapper.java): Final de CategoryMapper.java

--- backend/src/main/java/com/chema/db/backend/service/CategoryMapper.java
+++ backend/src/main/java/com/chema/db/backend/service/CategoryMapper.java
@@ -1,4 +1,24 @@
 package com.chema.db.backend.service;
 
+import com.chema.db.backend.dto.CategoryRequest;
+import com.chema.db.backend.dto.CategoryResponse;
+import com.chema.db.backend.model.Category;
+
 public class CategoryMapper {
+
+    public static Category toEntity(CategoryRequest request) {
+        Category category = new Category();
+        category.setName(request.getName());
+        category.setType(request.getType());
+        return category;
+    }
+
+    public static CategoryResponse toResponse(Category category) {
+        CategoryResponse response = new CategoryResponse();
+        response.setId(category.getId());
+        response.setName(category.getName());
+        response.setType(category.getType());
+        response.setSystem(category.getUser() == null);
+        return response;
+    }
 }
