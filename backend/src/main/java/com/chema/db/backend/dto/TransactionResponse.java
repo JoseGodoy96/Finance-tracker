986f96935a7e3622e5dd3eb1e423fd7639fb7b8a
Jose Maria <josemariagodoy1996@gmail.com>
2026-08-09 11:36:43 +0100

    <feat>(TransactionResponse.java): Final TransactionResponse

--- backend/src/main/java/com/chema/db/backend/dto/TransactionResponse.java
+++ backend/src/main/java/com/chema/db/backend/dto/TransactionResponse.java
@@ -1,4 +1,21 @@
 package com.chema.db.backend.dto;
 
+import com.chema.db.backend.model.TransactionType;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
 public class TransactionResponse {
+
+    private Long id;
+    private BigDecimal amount;
+    private LocalDate date;
+    private String description;
+    private TransactionType type;
+    private Long categoryId;
+    private String categoryName;
 }
