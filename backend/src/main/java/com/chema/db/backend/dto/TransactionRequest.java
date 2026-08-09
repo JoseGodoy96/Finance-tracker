a913e7d4d0f8526ed54df94009a7b2009b613a92
Jose Maria <josemariagodoy1996@gmail.com>
2026-08-09 11:31:43 +0100

    <feat>(TransactionRequest.java): Final TransactionRequest

--- backend/src/main/java/com/chema/db/backend/dto/TransactionRequest.java
+++ backend/src/main/java/com/chema/db/backend/dto/TransactionRequest.java
@@ -1,4 +1,30 @@
 package com.chema.db.backend.dto;
 
+import com.chema.db.backend.model.TransactionType;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
 public class TransactionRequest {
+
+    @NotNull
+    @Positive
+    private BigDecimal amount;
+
+    @NotNull
+    private LocalDate date;
+
+    @Size(max = 255)
+    private String description;
+
+    @NotNull
+    private TransactionType type;
+
 }
