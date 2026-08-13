fe2233437c455a9828367bafc5e1a3caf12abe77
Jose Maria <josemariagodoy1996@gmail.com>
2026-08-13 22:15:14 +0100

    <feat>(TransactionController.java): Modificacion para usar TransactionRequest y TransactionResponse

--- backend/src/main/java/com/chema/db/backend/controller/TransactionController.java
+++ backend/src/main/java/com/chema/db/backend/controller/TransactionController.java
@@ -1,5 +1,7 @@
 package com.chema.db.backend.controller;
 
+import com.chema.db.backend.dto.TransactionRequest;
+import com.chema.db.backend.dto.TransactionResponse;
 import com.chema.db.backend.model.Transaction;
 import com.chema.db.backend.model.User;
 import com.chema.db.backend.service.TransactionService;
@@ -18,23 +20,23 @@ public class TransactionController {
         this.transactionService = transactionService;
     }
 
-     @GetMapping
-    public List<Transaction> findAll(@AuthenticationPrincipal User user) {
+    @GetMapping
+    public List<TransactionResponse> findAll(@AuthenticationPrincipal User user) {
         return transactionService.findAllForUser(user);
     }
 
     @GetMapping("/{id}")
-    public Transaction findById(@PathVariable Long id, @AuthenticationPrincipal User user) {
+    public TransactionResponse findById(@PathVariable Long id, @AuthenticationPrincipal User user) {
         return transactionService.findByIdForUser(id, user);
     }
 
     @PostMapping
-    public Transaction create(@Valid @RequestBody Transaction transaction, @RequestParam Long categoryId , @AuthenticationPrincipal User user) {
-        return transactionService.createForUser(transaction, categoryId, user);
+    public TransactionResponse create(@Valid @RequestBody TransactionRequest request, @RequestParam Long categoryId , @AuthenticationPrincipal User user) {
+        return transactionService.createForUser(request, categoryId, user);
     }
 
     @PutMapping("/{id}")
-    public Transaction update(@PathVariable Long id, @Valid @RequestBody Transaction updated, @RequestParam Long categoryId, @AuthenticationPrincipal User user) {
+    public TransactionResponse update(@PathVariable Long id, @Valid @RequestBody TransactionRequest updated, @RequestParam Long categoryId, @AuthenticationPrincipal User user) {
         return transactionService.updateForUser(id, updated, categoryId, user);
     }
 
