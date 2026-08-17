87285195e159995a3af642537eeaafeeee76da36
Jose Maria <josemariagodoy1996@gmail.com>
2026-08-17 20:02:04 +0100

    <feat>(InvalidCredentialsException.java): Final de InvalidCredentialsException

--- backend/src/main/java/com/chema/db/backend/exception/InvalidCredentialsException.java
+++ backend/src/main/java/com/chema/db/backend/exception/InvalidCredentialsException.java
@@ -1,4 +1,7 @@
 package com.chema.db.backend.exception;
 
-public class InvalidCredentialsException {
+public class InvalidCredentialsException extends RuntimeException {
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
 }
