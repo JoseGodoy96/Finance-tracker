66151d03a55173bf972d4ec3aaa2e980ca6ab333
Jose Maria <josemariagodoy1996@gmail.com>
2026-08-17 20:06:57 +0100

    <feat>(ForbiddenAccessException.java): Final de ForbiddenAccessException

--- backend/src/main/java/com/chema/db/backend/exception/ForbiddenAccessException.java
+++ backend/src/main/java/com/chema/db/backend/exception/ForbiddenAccessException.java
@@ -1,4 +1,7 @@
 package com.chema.db.backend.exception;
 
-public class ForbiddenAccessException {
+public class ForbiddenAccessException extends RuntimeException {
+    public ForbiddenAccessException(String message) {
+        super(message);
+    }
 }
