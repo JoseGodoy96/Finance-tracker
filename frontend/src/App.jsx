fd711810f4b990d04ba3fe832f5aed78cc096ead
Jose Maria <josemariagodoy1996@gmail.com>
2026-08-31 11:57:59 +0100

    <feat>(App.jsx): Introduccion de LoginPage en App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Navigate to="/login" replace/>} />
-				<Route path="/login" element={<div>Login (todavia por hacer)</div>} />
+				<Route path="/login" element={<LoginPage />} />
 				<Route path="/register" element={<div>Register (todavia por hacer)</div>} />
 				<Route path="/dashboard" element={<div>Dashboard (todavia por hacer)</div>} />
 				<Route path="*" element={<div>404 - Página no encontrada</div>} />
