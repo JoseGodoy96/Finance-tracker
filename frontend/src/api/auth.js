b40fa8ef62dc3fa5cb6013a5111c83384e2a2eea
Jose Maria <josemariagodoy1996@gmail.com>
2026-09-03 12:46:59 +0200

    <feat>(auth.js): Cambiada la logica de Login y incluida la nueva logica para register

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,14 +1,26 @@
-const API_URL = 'http://localhost:8080/api/auth';
+import { apiFetch } from "./http";
 
 export async function login(username, password) {
-	const response = await fetch(`${API_URL}/login`, {
-	method: 'POST',
-	headers: { 'Content-Type': 'application/json' },
-	body: JSON.stringify({ username, password }),
+	const response = await apiFetch("/api/auth/login", {
+		method: 'POST',
+		body: JSON.stringify({ username, password })
     });
 
 	if (!response.ok) {
 		throw new Error('Login failed');
+	}
+
+	return await response.json();
+}
+
+export async function register(username, email, password) {
+	const response = await apiFetch("/api/auth/register", {
+		method: 'POST',
+		body: JSON.stringify({ username, email, password })
+	});
+
+	if (!response.ok) {
+		throw new Error('Register failed');
 	}
 
 	return await response.json();
