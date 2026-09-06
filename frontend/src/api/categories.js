c9852f673186a244b85b5c162600bbeb0a815491
José María <josemariagodoy1996@gmail.com>
2026-09-06 21:53:20 +0200

    <feat>(categories.js): Final de categories.js

---
+++ frontend/src/api/categories.js
@@ -0,0 +1,34 @@
+import { apiFetch } from "./http";
+
+export const getCategories = async () => {
+	const response = await apiFetch("/api/categories");
+	if (!response.ok) {
+		throw new Error("Error");
+	}
+	return await response.json();
+}
+
+export const createCategory = async (name, type) => {
+	
+	const response = await apiFetch("/api/categories", {
+		method: 'POST',
+		body: JSON.stringify({ name, type }),
+	});
+	if (!response.ok) {
+		throw new Error('Create category failed');
+	}
+	
+	return await response.json();
+}
+
+export const deleteCategory = async (id) => {
+
+	const response = await apiFetch(`/api/categories/${id}`, {
+		method: 'DELETE'
+	});
+	if (!response.ok) {
+		throw new Error('Delete category failed');
+	}
+
+	return;
+}
