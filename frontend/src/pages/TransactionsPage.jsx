832ec0983153a285a1c58425f7e09bed12dabd2f
José María <josemariagodoy1996@gmail.com>
2026-09-07 16:10:25 +0200

    <feat>(TransactionPage): Funcion de handle submit y creacion del form

--- frontend/src/pages/TransactionsPage.jsx
+++ frontend/src/pages/TransactionsPage.jsx
@@ -5,6 +5,11 @@ import { getTransactions, createTransaction, updateTransaction, deleteTransactio
 function TransactionsPage() {
 	const [transactions, setTransactions] = useState([]);
 	const [categories, setCategories] = useState([]);
+	const [amount, setAmount] = useState('');
+	const [date, setDate] = useState('');
+	const [description, setDescription] = useState('');
+	const [type, setType] = useState('EXPENSE');
+	const [categoryId, setCategoryId] = useState('');
 
 	useEffect(() => {
 		async function loadTransactions() {
@@ -19,9 +24,71 @@ function TransactionsPage() {
 		loadCategories();
 	}, [])
 
+	async function handleSubmit(e) {
+		e.preventDefault();
+
+		try {
+			const transaction = {
+				amount: parseFloat(amount),
+				date,
+				description,
+				type,
+			};
+			const data = await createTransaction(transaction, categoryId);
+			setTransactions([...transactions, data]);
+			setAmount('');
+			setDate('');
+			setDescription('');
+			setType('EXPENSE');
+			setCategoryId('');
+		} catch (err) {
+			alert('No se pudo crear la Transaction' + err);
+		}
+	}
+
 	return (
 		<div style={{ maxWidth: 600, margin: '2rem auto', padding: '1rem'}}>
 			<h1>Transactions</h1>
+
+			<form onSubmit={handleSubmit}>
+				<input 
+					type="number"
+					step="0.01"
+					value={amount}
+					onChange={(e) => setAmount(e.target.value)}
+					required/>
+				<input 
+					type="date"
+					value={date}
+					onChange={(e) => setDate(e.target.value)}
+					required/>
+				<input 
+					type="text" 
+					value={description}
+					onChange={(e) => setDescription(e.target.value)}
+					required/>
+				<select 
+					value={type}
+					onChange={(e) => setType(e.target.value)}
+					>
+					<option value="INCOME">INCOME</option>
+					<option value="EXPENSE">EXPENSE</option>
+				</select>
+				<select
+					value={categoryId}
+					onChange={(e) => setCategoryId(e.target.value)}
+					required
+					>
+					<option value="">-- Elige categoría --</option>
+					{categories.map(c => (
+						<option key={c.id} value={c.id}>
+							{c.name} ({c.type})
+						</option>
+					))}
+				</select>
+				<button type="submit">Crear</button>
+			</form>
+
 			<ul>
 				{transactions.map(t => (
 					<li key={t.id}>
