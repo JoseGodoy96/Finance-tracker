55fb0de9b9d2409a8c16a94794874cd1b444eca2
José María <josemariagodoy1996@gmail.com>
2026-09-07 15:34:13 +0200

    <feat>(TransactionsPage.jsx): Creando el bloque de Html y las dos funciones del useEffect

--- frontend/src/pages/TransactionsPage.jsx
+++ frontend/src/pages/TransactionsPage.jsx
@@ -1,12 +1,37 @@
 import { useState, useEffect } from "react";
 import { getCategories } from "../api/categories";
-import { getTransaction, createTransaction, updateTransaction, deleteTransaction } from "../api/transactions";
+import { getTransactions, createTransaction, updateTransaction, deleteTransaction } from "../api/transactions";
 
 function TransactionsPage() {
+	const [transactions, setTransactions] = useState([]);
+	const [categories, setCategories] = useState([]);
+
+	useEffect(() => {
+		async function loadTransactions() {
+			const data = await getTransactions();
+			setTransactions(data);
+		}
+		async function loadCategories() {
+			const data = await getCategories();
+			setCategories(data);
+		}
+		loadTransactions();
+		loadCategories();
+	}, [])
 
 	return (
-		<h1>Transactions</h1>
-	)
+		<div style={{ maxWidth: 600, margin: '2rem auto', padding: '1rem'}}>
+			<h1>Transactions</h1>
+			<ul>
+				{transactions.map(t => (
+					<li key={t.id}>
+						{t.date} - {t.amount} - {t.description}
+						<small> ({t.type} / {t.categoryName}) </small>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default TransactionsPage;
